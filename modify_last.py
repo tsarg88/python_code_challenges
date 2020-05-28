c67e47296388e5632513f9d390ffe39e8e93bade
umbur <work1415@gmail.com>
2020-05-28 16:33:02 -0600

    completes modify_last

---
+++ modify_last.py
@@ -0,0 +1,9 @@
+# Create a function which makes the last character of a string repeat n number of times.
+# Example:
+# modify_last("Hello", 3) ➞ "Hellooo"
+# modify_last("hey", 6) ➞ "heyyyyyy"
+
+def modify_last(txt, n):
+    return txt[:-1] + (txt[-1] * n)
+print(modify_last("Hello", 3))
+print(modify_last("hey", 6))
