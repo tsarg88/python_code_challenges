2258a84ad9498d26de6ac6c8dfa06fc44cea7c45
umbur <work1415@gmail.com>
2020-06-04 16:39:01 -0600

    completes pH_name

---
+++ pH_name.py
@@ -0,0 +1,22 @@
+# Given a pH value, return whether that value is "alkaline", "acidic", or "neutral". Return "invalid" if the value given is less than 0 or greater 
+# than 14.
+# Notes:
+# Values such as 6.9999 and 7.00001 should return "acidic" and "alkaline" respectively.
+# Example:
+# pH_name(5) ➞ "acidic"
+# pH_name(8.7) ➞ "alkaline"
+# pH_name(7) ➞ "neutral"
+
+def pH_name(pH):
+    if pH < 7 and pH >= 0:
+        return 'acidic'
+    if pH == 7:
+        return 'neutral'
+    if pH > 7 and pH <= 14:
+        return 'alkaline'
+    else:
+        return 'invalid'
+print(pH_name(5))
+print(pH_name(8.7))
+print(pH_name(7))
+print(pH_name(14.1))
