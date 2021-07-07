8305d58d3e53e5a2835fb6ace6e82022d4584326
umbur <work1415@gmail.com>
2021-07-07 17:55:52 -0600

    completes count_overlapping

--- py_draft.py
+++ py_draft.py
@@ -793,13 +793,34 @@ def findMergeNode(head1, head2):
    
 # print(largest_even([3, 7, 5]))
 
-def face_interval(num):
-    if type(num) is str:
-        return ":/"
-    difference = max(num) - min(num)
-    if difference in num:
-        return ":)"
-    else:
-        return ":("
+# def face_interval(num):
+#     if type(num) is str:
+#         return ":/"
+#     difference = max(num) - min(num)
+#     if difference in num:
+#         return ":)"
+#     else:
+#         return ":("
 
-print(face_interval("sd"))
+# print(face_interval("sd"))
+
+# Create a function that takes in a list of intervals and returns how many intervals overlap with 
+# a given point.
+# An interval overlaps a particular point if the point exists inside the interval, or on the interval's 
+# boundary. For example the point 3 overlaps with the interval [2, 4] (it is inside) and [2, 3] (it is on the boundary).
+
+# To illustrate:
+# count_overlapping([[1, 2], [2, 3], [1, 3], [4, 5], [0, 1]], 2) ➞ 3
+# # Since [1, 2], [2, 3] and [1, 3] all overlap with point 2
+# Examples
+# count_overlapping([[1, 2], [2, 3], [3, 4]], 5) ➞ 0
+# count_overlapping([[1, 2], [5, 6], [5, 7]], 5) ➞ 2
+# count_overlapping([[1, 2], [5, 8], [6, 9]], 7) ➞ 2
+
+def count_overlapping(intervals, point):
+    total = 0
+    for i in intervals:
+        if point >= i[0] and point <= i[1]:
+            total += 1
+    return total
+print(count_overlapping([[1, 2], [5, 6], [5, 7]], 5) )
