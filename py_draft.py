66f715038ac81bde7b4de7c81f40f38299a778e9
umbur <work1415@gmail.com>
2021-07-08 11:10:21 -0600

    completes mystery_func

--- py_draft.py
+++ py_draft.py
@@ -855,9 +855,26 @@ def findMergeNode(head1, head2):
 # mystery_func(19) ➞ 9
 # mystery_func(133) ➞ 9
 # test
+
+# def mystery_func(num):
+# 	prod = 1
+# 	for i in str(num):
+# 		prod *= int(i)
+# 	return prod   
+# print(mystery_func(152))        
+
+# This is a reverse coding challenge. Normally you're given explicit directions with how to create a function. 
+# Here, you must generate your own function to satisfy the relationship between the inputs and outputs.
+# Your task is to create a function that, when fed the inputs below, produces the sample outputs shown.
+# Examples
+# mystery_func(152) ➞ 10
+# mystery_func(832) ➞ 48
+# mystery_func(19) ➞ 9
+# mystery_func(133) ➞ 9
+
 def mystery_func(num):
 	prod = 1
 	for i in str(num):
 		prod *= int(i)
-	return prod   
-print(mystery_func(152))        
+	return prod
+print(mystery_func(152))
